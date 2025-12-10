5d094c5c0cef33d17727b86a38c8d859532daba9
DS1003 <mouhaleecr7@gmail.com>
2025-12-10 11:21:32 +0000

    Update admin profile image in prisma seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -23,7 +23,7 @@ async function main() {
       email: "admin@moomel.com",
       password: await hash("admin1234", 10),
       role: "ADMIN",
-      image: "https://api.dicebear.com/7.x/avataaars/svg?seed=Admin",
+      image: "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
       xp: 1000,
       level: 10,
     },
